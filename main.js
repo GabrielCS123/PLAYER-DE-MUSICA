17043bebb5950180f291703809a11ed84b414cea
GabrielCS123 <bielsouzacordeiro28@gmail.com>
2024-04-23 23:14:41 -0400

    player funcional - v1

---
+++ main.js
@@ -0,0 +1,37 @@
+function play(seletor) {
+    document.querySelector(seletor).play();
+}
+
+function pause(seletor) {
+    document.querySelector(seletor).pause();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclasp = document.querySelectorAll('.teclap');
+
+for (let index = 0; index < listaDeTeclas.length; index++) {
+    
+    const tecla = listaDeTeclas[index];
+    const teclap = tecla.classList[1];
+    const idAudio = `#musica_${teclap}`;
+
+    tecla.onclick = function () {
+        pause(idAudio);
+    }
+
+    tecla.onclick = function () {
+        play(idAudio);
+    }
+}
+
+
+for (let index = 0; index < listaDeTeclasp.length; index++) {
+    
+    const tecla = listaDeTeclasp[index];
+    const teclap = tecla.classList[1];
+    const idAudio = `#musica_${teclap}`;
+
+    tecla.onclick = function () {
+        pause(idAudio);
+    }
+}
